9be8f74366db2f084b33620c32d245c2acb9cf9c
祁瑞东 <Chairmanrdq@gmail.com>
2025-05-09 12:15:42 +0000

    分割线增加一些优雅的图标

--- src/components/ui/separator.tsx
+++ src/components/ui/separator.tsx
@@ -6,25 +6,52 @@ import * as SeparatorPrimitive from "@radix-ui/react-separator"
 import { cn } from "@/lib/utils"
 
 const Separator = React.forwardRef<
-  React.ElementRef<typeof SeparatorPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof SeparatorPrimitive.Root>
+  HTMLDivElement, // Changed to HTMLDivElement as it can be a div or SeparatorPrimitive.Root (which is also a div)
+  React.ComponentPropsWithoutRef<typeof SeparatorPrimitive.Root> & {
+    children?: React.ReactNode // Allow children for icon-in-middle style
+  }
 >(
   (
-    { className, orientation = "horizontal", decorative = true, ...props },
+    { className, orientation = "horizontal", decorative = true, children, ...props },
     ref
-  ) => (
-    <SeparatorPrimitive.Root
-      ref={ref}
-      decorative={decorative}
-      orientation={orientation}
-      className={cn(
-        "shrink-0 bg-border",
-        orientation === "horizontal" ? "h-[1px] w-full" : "h-full w-[1px]",
-        className
-      )}
-      {...props}
-    />
-  )
+  ) => {
+    // If children are provided and orientation is horizontal, render the "icon-in-middle" style
+    if (children && orientation === "horizontal") {
+      return (
+        <div
+          ref={ref}
+          role={decorative ? "none" : "separator"} // Apply ARIA role
+          aria-orientation="horizontal"
+          className={cn(
+            "flex w-full items-center", // Use flex to layout lines and content
+            className
+          )}
+          {...props}
+        >
+          <div className="flex-grow border-t border-border" /> {/* Left line */}
+          <span className="mx-2 shrink-0 text-muted-foreground"> {/* Content in the middle */}
+            {children}
+          </span>
+          <div className="flex-grow border-t border-border" /> {/* Right line */}
+        </div>
+      )
+    }
+
+    // Original Separator for vertical lines or horizontal lines without children
+    return (
+      <SeparatorPrimitive.Root
+        ref={ref}
+        decorative={decorative}
+        orientation={orientation}
+        className={cn(
+          "shrink-0 bg-border",
+          orientation === "horizontal" ? "h-[1px] w-full" : "h-full w-[1px]",
+          className
+        )}
+        {...props}
+      />
+    )
+  }
 )
 Separator.displayName = SeparatorPrimitive.Root.displayName
 
